3b8d14a08e3f4f9a45ae5cfc81e3bbb2a725cc06
Roman Kolisnyk <romkolisnyk@gmail.com>
2019-02-14 21:15:31 +0200

    swipe gestures + ES6 rewrited

--- js/compiled/common.js
+++ js/compiled/common.js
@@ -1,12 +1,10 @@
-"use strict"
-
-// preloader
+"use strict"; // preloader
 
 var preload = document.querySelector(".loader");
 
 function fadeOut(el) {
   el.style.opacity = 1;
-  var interval = setInterval(() => {
+  var interval = setInterval(function () {
     el.style.opacity = el.style.opacity - 0.05;
 
     if (el.style.opacity <= 0.05) {
@@ -16,38 +14,34 @@ function fadeOut(el) {
   }, 16);
 }
 
-window.onload = () => {
+window.onload = function () {
   setTimeout(function () {
     fadeOut(preload);
   }, 100);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", function () {
   // typing animation
-
   var string = "Hi there! I am Roman Kolisnyk, web-developer from Kyiv.";
   var str = string.split("");
   var el = document.querySelector('.sidebar .info');
+
   (function animate() {
     str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
     var running = setTimeout(animate, 90);
-  })();
+  })(); // menu button
 
-  // menu button
 
   var menuButton = document.querySelector('.sidebar__button');
   var sidebar = document.querySelector('.sidebar');
-  menuButton.addEventListener('click', () => {
+  menuButton.addEventListener('click', function () {
     menuButton.classList.toggle("open");
     sidebar.classList.toggle("open");
-  });
-
-  // tabs switcher
+  }); // tabs switcher
 
   function onTabClick(event) {
     var activeTabs = document.querySelectorAll('.active');
-    activeTabs.forEach((tab) => {
+    activeTabs.forEach(function (tab) {
       tab.className = tab.className.replace('active', '');
     });
     event.target.className += ' active';
@@ -55,10 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   var element = document.querySelector('.tabs');
-  element.addEventListener('click', onTabClick, false);
-
-
-  // clock
+  element.addEventListener('click', onTabClick, false); // clock
 
   function startTime() {
     var today = new Date();
@@ -67,35 +58,34 @@ document.addEventListener("DOMContentLoaded", () => {
     m = checkTime(m);
     document.getElementById('clock').innerHTML = h + ":" + m;
     var t = setTimeout(startTime, 1000);
-  };
+  }
+
+  ;
 
   function checkTime(i) {
     if (i < 10) {
       i = "0" + i;
-    };
+    }
+
+    ;
     return i;
-  };
-  startTime();
+  }
 
+  ;
+  startTime(); // swipe
 
-
-  // swipe
-
-  let pageWidth = window.innerWidth || document.body.clientWidth;
-  let treshold = Math.max(1, Math.floor(0.01 * (pageWidth)));
-  let touchstartX = 0;
-  let touchstartY = 0;
-  let touchendX = 0;
-  let touchendY = 0;
-
-  const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
-  const gestureZone = document.querySelector('.main');
-
+  var pageWidth = window.innerWidth || document.body.clientWidth;
+  var treshold = Math.max(1, Math.floor(0.01 * pageWidth));
+  var touchstartX = 0;
+  var touchstartY = 0;
+  var touchendX = 0;
+  var touchendY = 0;
+  var limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+  var gestureZone = document.querySelector('.main');
   gestureZone.addEventListener('touchstart', function (event) {
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
   }, false);
-
   gestureZone.addEventListener('touchend', function (event) {
     touchendX = event.changedTouches[0].screenX;
     touchendY = event.changedTouches[0].screenY;
@@ -103,39 +93,32 @@ document.addEventListener("DOMContentLoaded", () => {
   }, false);
 
   function handleGesture(e) {
-    let x = touchendX - touchstartX;
-    let y = touchendY - touchstartY;
-    let yx = Math.abs(y / x);
+    var x = touchendX - touchstartX;
+    var y = touchendY - touchstartY;
+    var yx = Math.abs(y / x);
+
     if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
       if (yx <= limit) {
-
-        const tab = document.querySelector('.tabs .tab.active');
-        const tabs = document.querySelectorAll('.tabs .tab');
-        const section = document.querySelector('section.active');
+        var tab = document.querySelector('.tabs .tab.active');
+        var tabs = document.querySelectorAll('.tabs .tab');
+        var section = document.querySelector('section.active');
 
         if (x < 0 && tab != tabs[tabs.length - 1]) {
-
           tab.nextElementSibling.classList.add("active");
           tab.classList.remove("active");
-
           section.nextElementSibling.classList.add("active");
           section.classList.remove("active", "right");
-
         }
-        if (x > 0 && tab != tabs[0]) {
 
+        if (x > 0 && tab != tabs[0]) {
           tab.previousElementSibling.classList.add("active");
           tab.classList.remove("active");
-
           section.previousElementSibling.classList.add("active", "right");
           section.classList.remove("active", "right");
-
         }
       }
     }
+  }
 
-
-  };
-
-
+  ;
 });
