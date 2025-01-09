c13668de3ef220fa4eb54fe191089af905d348df
Roman Kolisnyk <ramapitek@Romans-MacBook-Pro.local>
2025-01-10 01:17:15 +0200

    chore: upd info & edit styles

--- src/main.js
+++ src/main.js
@@ -4,21 +4,21 @@ const htmlNode = document.querySelector("html");
 const themeSwitcherToggle = document.querySelector(".switcher");
 const themeSwitcherCheckbox = document.querySelector(".switcher__checkbox");
 
-const currentTheme = window.localStorage.getItem("theme");
+// const currentTheme = window.localStorage.getItem("theme");
 
 const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (
-    currentTheme === DARK_THEME &&
-    window.matchMedia("(prefers-color-scheme: dark)")?.matches
-  ) {
-    htmlNode.setAttribute("data-theme", DARK_THEME);
-    themeSwitcherCheckbox.checked = true;
-    themeSwitcherCheckbox.setAttribute("aria-checked", "true");
-  }
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   if (
+//     currentTheme === DARK_THEME &&
+//     window.matchMedia("(prefers-color-scheme: dark)")?.matches
+//   ) {
+//     htmlNode.setAttribute("data-theme", DARK_THEME);
+//     themeSwitcherCheckbox.checked = true;
+//     themeSwitcherCheckbox.setAttribute("aria-checked", "true");
+//   }
+// });
 
 themeSwitcherToggle.addEventListener("click", () => {
   if (themeSwitcherCheckbox.checked === false) {
